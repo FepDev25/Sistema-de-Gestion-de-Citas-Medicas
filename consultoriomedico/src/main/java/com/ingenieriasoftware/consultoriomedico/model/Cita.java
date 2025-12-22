c790bf17a16bc218bf66f3b391c548e734f65d86
EdisonAmendano <edison32314@gmail.com>
2025-12-22 14:59:56 -0500

    fix(core): agregar getters y setters en entidad Cita

--- consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/model/Cita.java
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/model/Cita.java
@@ -28,5 +28,61 @@ public class Cita {
     @JoinColumn(name = "medico_id")
     private Medico medico;
 
-    // Getters y setters
+    public Long getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(Long idCita) {
+        this.idCita = idCita;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    
 }
