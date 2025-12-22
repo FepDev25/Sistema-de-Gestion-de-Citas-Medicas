c14f21369c20473739fd0743aa8c5641e3137a29
SSuquilanda <ssuquilanda@est.ups.edu.ec>
2025-12-22 11:08:04 -0500

    Mejoras en la clase de medico

--- consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/model/Medico.java
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/model/Medico.java
@@ -6,15 +6,16 @@ import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+//Entidad que hereda de Usuario y comparte su tabla en BD
 @Entity
 //@Table(name = "medicos")
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = true)// verifica también los atributos heredados de Usuario
 public class Medico extends Usuario {
-
+    
     @Column(nullable = false, length = 100)
     private String especialidad;
-
+    //con esto evitamos que se repitan licencias en la BD
     @Column(nullable = false, unique = true, length = 50)
     private String licencia;
 }
