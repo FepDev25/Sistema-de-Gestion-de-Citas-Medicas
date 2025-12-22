ebd2237f4fcbc4af35a741345620f8ec7302ebd3
EdisonAmendano <edison32314@gmail.com>
2025-12-22 17:43:28 -0500

    feat(controller): endpoint para agendar citas

---
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/controller/CitaController.java
@@ -0,0 +1,39 @@
+package com.ingenieriasoftware.consultoriomedico.controller;
+
+import com.ingenieriasoftware.consultoriomedico.model.Cita;
+import com.ingenieriasoftware.consultoriomedico.service.CitaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/citas")
+public class CitaController {
+
+    private final CitaService citaService;
+
+    public CitaController(CitaService citaService) {
+        this.citaService = citaService;
+    }
+
+    @PostMapping("/agendar")
+    public ResponseEntity<Cita> agendarCita(
+            @RequestParam Long idMedico,
+            @RequestParam String cedulaPaciente,
+            @RequestParam LocalDate fecha,
+            @RequestParam LocalTime hora,
+            @RequestParam Integer duracion
+    ) {
+        Cita cita = citaService.crearCita(
+                idMedico,
+                cedulaPaciente,
+                fecha,
+                hora,
+                duracion
+        );
+
+        return ResponseEntity.ok(cita);
+    }
+}
