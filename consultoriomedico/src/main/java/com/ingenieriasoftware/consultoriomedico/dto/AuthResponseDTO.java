8c29cb5bc596ac0bbf333f19dfe38c8a9c000619
Felipe <lamediatricolor@gmail.com>
2026-01-25 17:31:43 -0500

    Ft: Agregando clases dto para autenticacion

---
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package com.ingenieriasoftware.consultoriomedico.dto;
+
+public class AuthResponseDTO {
+    private String token;
+
+    public AuthResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+}
