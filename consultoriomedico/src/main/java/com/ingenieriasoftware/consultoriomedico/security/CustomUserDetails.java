13723c1497d30b31412640383b22af0e36fb5c23
Felipe <lamediatricolor@gmail.com>
2026-01-25 12:08:24 -0500

    Ft: Clase para envolver usuario y adaptarlo a UserDetails

---
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/security/CustomUserDetails.java
@@ -0,0 +1,46 @@
+package com.ingenieriasoftware.consultoriomedico.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.ingenieriasoftware.consultoriomedico.model.Usuario;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Usuario usuario;
+
+    public CustomUserDetails(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + usuario.getRol()));
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
