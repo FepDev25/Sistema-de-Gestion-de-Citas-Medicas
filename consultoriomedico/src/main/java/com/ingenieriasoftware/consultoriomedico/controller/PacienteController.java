4245d4bddebd9486dc44c9680518c35003d7d631
Felipe <lamediatricolor@gmail.com>
2026-01-25 10:45:05 -0500

    Ft: Actualizando controller de paciente para que trabaje con objeto dto

--- consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/controller/PacienteController.java
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/controller/PacienteController.java
@@ -1,9 +1,19 @@
 package com.ingenieriasoftware.consultoriomedico.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ingenieriasoftware.consultoriomedico.dto.PacienteRequestDTO;
 import com.ingenieriasoftware.consultoriomedico.model.Paciente;
 import com.ingenieriasoftware.consultoriomedico.service.PacienteService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -16,8 +26,18 @@ public class PacienteController {
     }
 
     @PostMapping
-    public Paciente crearPaciente(@RequestBody Paciente paciente) {
-        return pacienteService.crearPaciente(paciente);
+    public ResponseEntity<Paciente> crearPaciente(@Valid @RequestBody PacienteRequestDTO pacienteDTO) {
+        Paciente paciente = new Paciente();
+        paciente.setNombres(pacienteDTO.getNombres());
+        paciente.setApellidos(pacienteDTO.getApellidos());
+        paciente.setUsername(pacienteDTO.getUsername());
+        paciente.setPassword(pacienteDTO.getPassword());
+        paciente.setCedula(pacienteDTO.getCedula());
+        paciente.setTelefono(pacienteDTO.getTelefono());
+        paciente.setDireccion(pacienteDTO.getDireccion());
+        paciente.setFechaNacimiento(pacienteDTO.getFechaNacimiento());
+
+        return new ResponseEntity<>(pacienteService.crearPaciente(paciente), HttpStatus.CREATED);
     }
 
     @GetMapping("/{cedula}")
