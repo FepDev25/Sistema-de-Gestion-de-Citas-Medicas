7ce627914cc9510f923f9a723b6eb72d4437a78d
Felipe <lamediatricolor@gmail.com>
2026-01-25 10:44:55 -0500

    Ft: Actualizando controller de medico para que trabaje con objeto dto

--- consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/controller/MedicoController.java
+++ consultoriomedico/src/main/java/com/ingenieriasoftware/consultoriomedico/controller/MedicoController.java
@@ -1,9 +1,21 @@
 package com.ingenieriasoftware.consultoriomedico.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ingenieriasoftware.consultoriomedico.dto.MedicoRequestDTO;
 import com.ingenieriasoftware.consultoriomedico.model.Medico;
 import com.ingenieriasoftware.consultoriomedico.service.MedicoService;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/medicos")
@@ -16,8 +28,16 @@ public class MedicoController {
     }
     
     @PostMapping
-    public Medico crear(@RequestBody Medico medico) {
-        return medicoService.registrarMedico(medico);
+    public ResponseEntity<Medico> crear(@Valid @RequestBody MedicoRequestDTO medicoDTO) {
+        Medico medico = new Medico();
+        medico.setNombres(medicoDTO.getNombres());
+        medico.setApellidos(medicoDTO.getApellidos());
+        medico.setUsername(medicoDTO.getUsername());
+        medico.setPassword(medicoDTO.getPassword());
+        medico.setEspecialidad(medicoDTO.getEspecialidad());
+        medico.setLicencia(medicoDTO.getLicencia());
+
+        return new ResponseEntity<>(medicoService.registrarMedico(medico), HttpStatus.CREATED);
     }
 
     @GetMapping
